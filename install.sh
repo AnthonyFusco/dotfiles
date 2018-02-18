8a5cdacbff2315d4c2c4a3fa23bbdebe933f791e
Anthony Fusco <anthony.fusco42@gmail.com>
2018-02-18 16:33:32 +0100

    corrected typo

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sfv $DOTFILES_DIR/.zshrc ~
 ln -sfv $DOTFILES_DIR/.tmux.conf ~
 ln -sfv $DOTFILES_DIR/.bashrc ~
-ln -sfv $DOTFILES_DIR/.bashr_profile ~
+ln -sfv $DOTFILES_DIR/.bash_profile ~
 ln -sfv $DOTFILES_DIR/.gitconfig ~
 ln -sfv $DOTFILES_DIR/.vimrc ~/.vim
