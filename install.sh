d3c4251706eceec601cd00893ee3aad2cd798f0b
Anthony Fusco <anthony.fusco42@gmail.com>
2023-05-05 16:21:02 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-ln -sfv $DOTFILES_DIR/.zshrc ~
-ln -sfv $DOTFILES_DIR/.tmux.conf ~
-ln -sfv $DOTFILES_DIR/.bashrc ~
-ln -sfv $DOTFILES_DIR/.bash_profile ~
-ln -sfv $DOTFILES_DIR/.gitconfig ~
-ln -sfv $DOTFILES_DIR/.vimrc ~
